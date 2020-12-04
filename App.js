507c946f4071f521751286620b0677e735aa689c
junjie <jun_jie1994@hotmail.com>
2020-12-04 11:12:22 +0800

    add events screens

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from "./screens/HomeScreen";
+import EventsScreen from "./screens/EventsScreen";
+import ContactScreen from "./screens/ContactScreen";
+import Ionicons from "react-native-vector-icons/Ionicons";
+
+
+const Tab = createBottomTabNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator 
+              screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                  let iconName;
+      
+                  if (route.name === 'Home') {
+                    iconName = 'md-home';
+                  } else if (route.name === 'Events') {
+                    iconName = 'ios-rocket';
+                  } else if (route.name === 'Contact') {
+                    iconName = 'ios-people';
+                  }
+      
+                  // You can return any component that you like here!
+                  return <Ionicons name={iconName} size={size} color={color} />;
+                },
+              })}
+              tabBarOptions={{
+                activeTintColor: 'tomato',
+                inactiveTintColor: 'gray',
+              }}>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Events" component={EventsScreen} />
+        <Tab.Screen name="Contact" component={ContactScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/EventsScreen.js
@@ -0,0 +1,34 @@
+  
+import React from "react";
+import { Text, View, Button } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+function EventsHomeScreen({navigation}) {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Welcome to my event!</Text>
+      <Button title="Second screen" onPress={ () => navigation.navigate("EventsSecond")}>
+    </Button>
+    </View>
+  );
+}
+
+function EventsSecondScreen(){
+    return (
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+          <Text>Events 2nd screen!</Text>
+        </View>
+      );
+    }
+
+const Stack = createStackNavigator();
+
+export default function EventsScreen() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="EventsHome" component={EventsHomeScreen} />
+      <Stack.Screen name="EventsSecond" component={EventsSecondScreen} />
+
+    </Stack.Navigator>
+  );
+}
