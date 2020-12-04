3a2e0ead0bca6b6a8528e90e9451c2435209b44b
junjie <jun_jie1994@hotmail.com>
2020-12-04 12:51:47 +0800

    with random profiles

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,84 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
+import React from "react";
+import { View, StyleSheet, Image } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import {
+  Text,
+  Card,
+  CardItem,
+  Left,
+  Thumbnail,
+  Body,
+  Button,
+  Icon,
+  Accordion,
+} from "native-base";
 
-export default function HomeScreen() {
+const dataArray = [
+  {
+    title: "What is this?",
+    content: "Exactly what the title says. A potato exhibition!",
+  },
+  {
+    title: "Who is this by?",
+    content: "The International Society of Potato Growers.",
+  },
+  {
+    title: "Why is this?",
+    content: "Because potatoes! Wheee!",
+  },
+];
+
+function HomeMainScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
+    <View style={{ padding: 10 }}>
+      <Text style={styles.titleText}>International Potato Day</Text>
+      <Card style={styles.card}>
+        <CardItem>
+          <Body>
+            <Text style={styles.cardTitle}>Welcome</Text>
+            <Text>
+              Thanks for downloading our app! Here, you'll find all kinds of
+              information about our upcoming event. It'll be great!
+            </Text>
+          </Body>
+        </CardItem>
+        <CardItem cardBody>
+          <Image
+            source={{
+              uri:
+                "https://upload.wikimedia.org/wikipedia/commons/a/ab/Patates.jpg",
+            }}
+            style={{ height: 200, width: null, flex: 1 }}
+          />
+        </CardItem>
+      </Card>
+
+      <Accordion style={styles.accordion} dataArray={dataArray} expanded={0} />
     </View>
   );
 }
+
+const Stack = createStackNavigator();
+
+export default function HomeScreen() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeMainScreen} />
+    </Stack.Navigator>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleText: {
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  cardTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+});
